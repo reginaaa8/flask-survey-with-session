0f16b41f1378bd4562f3a80097a478a54d7e041a
Regina Steffen <reginasteffen@live.com>
2024-06-14 19:12:35 -0500

    rewritten program using session

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, redirect, flash
+from flask import Flask, request, render_template, redirect, flash, session 
 from flask_debugtoolbar import DebugToolbarExtension
 from surveys import satisfaction_survey as survey
 
@@ -10,7 +10,6 @@ app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 debug = DebugToolbarExtension(app)
 
-RESPONSES = []
 
 @app.route('/')
 def index():
@@ -20,28 +19,30 @@ def index():
 @app.route('/start-survey', methods=['POST'])
 def start_survey():
     '''clear responses from "database" before beginning survey'''
-    RESPONSES.clear()
-    return redirect(f'/questions/{len(RESPONSES)}')
+    session['responses'] = []
+    return redirect(f'/questions/{len(session['responses'])}')
 
 @app.route('/questions/<id>')
 def show_question(id):
     '''show user current question'''
-    if int(id) != len(RESPONSES):
+    if int(id) != len(session['responses']):
         #redirect to current question if user tries to access questions out of order
         flash(f'INVALID QUESTION ID: {id}')
-        return redirect(f'/questions/{len(RESPONSES)}')
-    id = len(RESPONSES)
-    if len(RESPONSES) == len(survey.questions):
+        return redirect(f'/questions/{len(session['responses'])}')
+    id = len(session['responses'])
+    if len(session['responses']) == len(survey.questions):
         # when survey is complete, show user 'thank you' page
         return redirect('/completed')
     return render_template('questions.html', survey=survey, questions=survey.questions[id], id=id)
 
 @app.route('/response', methods=['POST'])
 def handle_response():
-    '''add user response to RESPONSES (aka my fake db) and redirect to next question'''
+    '''add user response to session['responses'] (aka my fake db) and redirect to next question'''
+    responses = session['responses']
     response = request.form['response']
-    RESPONSES.append(response)
-    return redirect(f'/questions/{len(RESPONSES)}')
+    responses.append(response)
+    session['responses'] = responses
+    return redirect(f'/questions/{len(session['responses'])}')
 
 @app.route('/completed')
 def complete():
